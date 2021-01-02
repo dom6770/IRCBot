c1a5b791a6fbe6fda16ad08ce848e672b30df380
Dominik Pflegerl <github.me@neus.xyz>
2021-01-02 20:44:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IRC
+{
+    class Bot
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
